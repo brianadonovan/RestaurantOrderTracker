1f3fc4b8be928b51760bd8764ba66ea0745da468
bre <briana.r.donovan@gmail.com>
2018-09-09 17:19:44 -0700

    use heroku db if present

--- app.js
+++ app.js
@@ -27,15 +27,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', routes);
 app.use('/users', usersRouter);
 
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring =
+    process.env.MONGODB_URI || config.db;
 
-function _initializeModels (){
-    mongoose.connect(config.db);
-    mongoose.connection.on('error', function(err) {
-        console.log('Mongodb failed to connect', {err: err});
-    })
-}
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
 
-_initializeModels();
+// Makes connection asynchronously. Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+    if (err) {
+        console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+        console.log ('Succeeded connected to: ' + uristring);
+    }
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
